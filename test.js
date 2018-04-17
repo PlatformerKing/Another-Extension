5ea5ee51d5bc117d1757ce0566b263bee8bad11f
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-17 16:55:06 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,15 @@ var tornado = function() {
     var d=document, s = d.createElement("style");
     s.setAttribute("type","text/css"), d.head.appendChild(s), s.innerHTML="*{-webkit-transform:rotate(99999deg);-ms-transform:rotate(99999deg);transform:rotate(9999deg);transition-duration:30s;}";
 };
-var ll = document.getElementsByClassName("user-card-edit-menu");
+var main = function() {
+    if(!document.body) {
+        main();
+    }
+    var ll = document.getElementsByClassName("user-card-edit-menu");
 var para = document.createElement("p");
 var node = document.createTextNode("Testing more");
 para.appendChild(node);
 ll.appendChild(para);
+};
+main();
+
