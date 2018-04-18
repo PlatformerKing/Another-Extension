646af95f9784cf96e7d292053908acea4ff1dc61
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-18 09:14:39 -0400

    Update test.js

--- test.js
+++ test.js
@@ -11,5 +11,5 @@ var go = function() {
     var x = document.getElementById('bio-picker');
     x.value = 'CHEESE';
 };
-//document.getElementById('bio-picker').onkeyup = go;
-setInterval(go, 1000);
+document.getElementById('bio-picker').onkeypress = function(){go();};
+//setInterval(go, 1000);
