73fc11b705835545eaac5a60657fc22a86763edf
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-19 09:11:29 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var dummyTexts = [
     'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek\'s fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just flocked up to quiz and vex him. Adjusting quiver and bow, Zompyc[1] killed the fox. My faxed joke won a pager in the cable TV quiz show. Amazingly few discotheques provide jukeboxes. My girl wove six dozen plaid jackets before she quit. Six big devils from Japan quickly forgot how to waltz. Big July earthquakes confound zany experimental vow. Foxy parsons quiz and cajole the lovably dim wiki-girl. Have a pick: twenty six letters - no forcing a jumbled quiz! Crazy Fredericka bought many very exquisite opal jewels. Sixty zippers were quickly picked from the woven jute bag. A quick movement of the enemy will jeopardize six gunboats. All questions asked by five watch experts amazed the judge. Jack quietly moved up front and seized the big ball of wax.',
 ];
-var reactSpeed = 25;
 var charN = 0;
+var mode = 1;
 var setPos = function(caretPos) {
     var elem = document.activeElement;
     if(elem != null) {
@@ -29,13 +29,13 @@ document.onkeypress = function(e) {
         var val = el.value;
         var beg = val.slice(0, el.selectionStart-1);
         var end = val.slice(el.selectionStart, val.length);
-        //alert(beg+end);
-        el.value = beg+dummyTexts[0][charN]+end;
-        setPos(beg.length+2);
-        charN ++;
+        if (mode === 0) {
+            //alert(beg+end);
+            el.value = beg+dummyTexts[0][charN]+end;
+            setPos(beg.length+1);
+            charN ++;
+        }else if (mode === 1) {
+            setPos(0);
+        }
     }
 };
-
-//setInterval(function() {
-    //document.activeElement.value = dummyTexts[0].slice(0, document.activeElement.value.length);
-//}, reactSpeed);
