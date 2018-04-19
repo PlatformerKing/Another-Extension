291cc064ae140ad08a7aaf498aee70ad877932e6
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-19 10:01:34 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 function changeMode() {
-    alert('test');
     var m = document.getElementById('mode-select');
-    alert("Yay"+m.value);
+    console.log("Yay"+m.value);
     mode = m.value;
 };
 changeMode();
