0e18b60ec377d0322e544ae6f58bb2508e1c3aaa
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-19 17:44:56 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 function changeMode() {
-    var m = document.getElementById('mode-select');
+    var m = document.getElementById("mode-select");
     console.log("Yay"+m.value);
     mode = m.value;
 }
+console.log(document.getElementById("mode-select"));
 document.getElementById("mode-select").onchange = function() {
     alert('Yo');
     changeMode();
